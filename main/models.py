6d96b45ab1d7b5b40b1d0694fbc27c48c54e6e36
abylai <abok_003@mail.ru>
2023-02-12 23:01:48 +0600

    DZTG

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Kvart(models.Model):
+    description = models.TextField(blank=True)
+    size = models.CharField(max_length=255)
+    prise = models.IntegerField()
+    region = models.CharField(max_length=255)
+    volume = models.IntegerField()
+
+class Users(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+
+class Comments(models.Model):
+    comment = models.TextField(blank=True)
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=50)
+class Roles(models.Model ):
+    name = models.CharField(max_length=50)
+
+
+
+
+
--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,5 @@ from .views import *
 urlpatterns = [
   path('', index , name='home'),
   path('cats/<int:catid>/', categories),
-  # path('errors403/', views.error403, name='403'),
-  # path('errors404/', views.error403, name='404'),
-  # path('errors400/', views.error403, name='400'),
-  # path('errors500/', views.error403, name='500')
 
 ]
